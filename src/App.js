1ae4fda3ced0aef5b326118fcad50d3aa0794e86
Sanjana92005 <sanjana.naik911@gmail.com.com>
2026-05-05 22:15:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Studentcrud from './component/Studentcrud';
+
+function App() {
+  return (
+    <div>
+      <Studentcrud />
+    </div>
+  );
+}
+
+export default App;
